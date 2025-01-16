9074283060d3fd6a19eb85f6fef2621202571d4d
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-16 20:14:09 -0300

    feat: add register request dto

---
+++ app/Domain/DTOs/RegisterRequestDTO.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Domain\DTOs;
+
+class RegisterRequestDTO
+{
+    public function __construct(
+        public readonly string $name,
+        public readonly string $lastname,
+        public readonly string $phone,
+        public readonly string $email,
+        public readonly string $password
+    ) {}
+
+}
