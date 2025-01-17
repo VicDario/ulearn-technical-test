a03ca7b018dd7afbed0ebc11ef1f035b8881b66f
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-17 18:06:09 -0300

    feat: add navbar component

---
+++ resources/js/Components/NavBar.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <nav class="bg-white shadow">
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div class="flex h-16 justify-between">
+                <div class="flex flex-shrink-0 items-center">
+                    <Link href="/">
+                        <ApplicationLogo />
+                    </Link>
+                </div>
+                <div class="flex items-center">
+                    <ResponsiveNavLink
+                        :href="route('logout')"
+                        method="post"
+                        as="button"
+                    >
+                        Log Out
+                    </ResponsiveNavLink>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
