cecc66f6e3496097dff1561647157e8d8839d3c7
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-17 17:40:28 -0300

    feat: accept label for

--- resources/js/Components/InputLabel.vue
+++ resources/js/Components/InputLabel.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 defineProps<{
+    labelFor: string;
     value?: string;
 }>();
 </script>
 
 <template>
-    <label class="block text-sm font-medium text-gray-700">
-        <span v-if="value">{{ value }}</span>
-        <span v-else><slot /></span>
+    <label :for="labelFor" class="block text-sm font-medium text-gray-700">
+        {{ value }}
     </label>
 </template>
