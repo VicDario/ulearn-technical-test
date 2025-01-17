0f5236f8dadce254fd632f28e2766855071b3c52
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-17 17:40:54 -0300

    feat: put placeholders and use spanish

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -6,16 +6,15 @@ import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import GuestLayout from '@/Layouts/GuestLayout.vue';
 import { Head, Link, useForm } from '@inertiajs/vue3';
+import { computed } from 'vue';
 
-defineProps<{
-    canResetPassword?: boolean;
+const props = defineProps<{
     status?: string;
 }>();
 
 const form = useForm({
     email: '',
     password: '',
-    remember: false,
 });
 
 const submit = () => {
@@ -25,20 +24,29 @@ const submit = () => {
         },
     });
 };
+
+const registeredMessage = computed<string | null>(() => {
+    if (props.status === 'User registered successfully')
+        return 'Usuario registrado existosamente. Por favor inicia sesión.';
+
+    return null;
+});
 </script>
 
 <template>
     <GuestLayout>
-        <Head title="Log in" />
+        <Head title="Iniciar Sesión" />
 
-        <div v-if="status" class="mb-4 text-sm font-medium text-green-600">
-            {{ status }}
+        <div
+            v-if="status"
+            class="mb-4 text-center text-sm font-medium text-green-600"
+        >
+            {{ registeredMessage }}
         </div>
 
         <form @submit.prevent="submit">
             <div>
-                <InputLabel for="email" value="Email" />
-
+                <InputLabel labelFor="email" value="Email" />
                 <TextInput
                     id="email"
                     type="email"
@@ -47,8 +55,8 @@ const submit = () => {
                     required
                     autofocus
                     autocomplete="username"
+                    placeholder="example@example.com"
                 />
-
                 <InputError class="mt-2" :message="form.errors.email" />
             </div>
 
@@ -62,33 +70,19 @@ const submit = () => {
                     v-model="form.password"
                     required
                     autocomplete="current-password"
+                    placeholder="*******"
                 />
 
                 <InputError class="mt-2" :message="form.errors.password" />
             </div>
 
-            <div class="mt-4 block">
-                <label class="flex items-center">
-                    <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ms-2 text-sm text-gray-600">Remember me</span>
-                </label>
-            </div>
-
             <div class="mt-4 flex items-center justify-end">
-                <Link
-                    v-if="canResetPassword"
-                    :href="route('password.request')"
-                    class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                    Forgot your password?
-                </Link>
-
                 <PrimaryButton
                     class="ms-4"
                     :class="{ 'opacity-25': form.processing }"
                     :disabled="form.processing"
                 >
-                    Log in
+                    Iniciar Sesión
                 </PrimaryButton>
             </div>
         </form>
