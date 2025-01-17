4841ff5a8532569641d7715b6d787216ca7741c7
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-17 18:05:41 -0300

    feat: delete unused routes and update dashboard route to profile

--- routes/web.php
+++ routes/web.php
@@ -8,18 +8,11 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'laravelVersion' => Application::VERSION,
-
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::get('/profile', function () {
+    return Inertia::render('Profile');
+})->middleware(['auth', 'verified'])->name('profile');
 
 require __DIR__.'/auth.php';
