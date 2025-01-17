b2f9c1bf03662ebe79f674f0117e60882afd7c11
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-17 16:18:15 -0300

    chore: delete unused controllers

--- routes/auth.php
+++ routes/auth.php
@@ -1,10 +1,6 @@
 <?php
 
 use Presentation\Http\Controllers\Auth\AuthenticatedSessionController;
-use Presentation\Http\Controllers\Auth\ConfirmablePasswordController;
-use Presentation\Http\Controllers\Auth\NewPasswordController;
-use Presentation\Http\Controllers\Auth\PasswordController;
-use Presentation\Http\Controllers\Auth\PasswordResetLinkController;
 use Presentation\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,28 +14,9 @@ Route::middleware('guest')->group(function () {
         ->name('login');
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
-
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-        ->name('password.request');
-
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-        ->name('password.email');
-
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-        ->name('password.reset');
-
-    Route::post('reset-password', [NewPasswordController::class, 'store'])
-        ->name('password.store');
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-        ->name('password.confirm');
-
-    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
-
-    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
-
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
 });
