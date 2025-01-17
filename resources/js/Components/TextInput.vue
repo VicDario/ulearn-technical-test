ee336bc59ad66d2f08b8003e284f9e0eab715379
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-17 17:40:02 -0300

    feat: modify styles for input

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -16,7 +16,7 @@ defineExpose({ focus: () => input.value?.focus() });
 
 <template>
     <input
-        class="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+        class="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
         v-model="model"
         ref="input"
     />
